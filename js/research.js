332d20bd2ed0584efd46af921b6da8dace3df3ea
Antony <antonyalbertraj1994@gmail.com>
2026-02-03 21:59:59 -0500

    Added research page

---
+++ js/research.js
@@ -0,0 +1,47 @@
+
+async function loadData() {
+    const div_research = document.getElementById("research-container");
+    console.log(`div_research:${div_research}`)
+
+    try{
+        const response = await fetch('research-paper-template.html');
+        if (!response.ok){
+            throw new Error("Network not ready")
+        } 
+        const htmlstring = await response.text()
+        const parser = new DOMParser()
+        const doc = parser.parseFromString(htmlstring,'text/html')
+
+        const template = doc.getElementById("paper-template")
+        if (!template)
+            return
+        
+        const response_paper = await fetch('paperlist.json')
+        if(!response_paper.ok){
+            return new Error("Cannot read json");
+        }
+
+        response_json = await response_paper.json()
+        response_json.forEach(paper => {
+            const clone = template.content.cloneNode(true);
+            clone.querySelector(".paper-title").textContent = paper.title 
+            clone.querySelector(".paper-authors").textContent = paper.authors 
+            clone.querySelector(".paper-venue").innerHTML = `${paper.venue} <b>(${paper.conference})</b>` 
+
+            clone.querySelector(".thumbnail").src = paper.thumbnailurl
+            console.log(`Researchjs:${paper.title}`)
+            // const div = document.createElement("div")
+            // div.innerHTML = `<h1>${paper.title}</h1>`;
+            div_research.appendChild(clone)
+        });
+
+    } catch(error) {
+        console.error("There is a problem with the fetch reponse")
+    }
+}
+
+loadData()
+
+
+
+
